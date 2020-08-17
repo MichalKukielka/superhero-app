fd699d3e57f3bc2f864acd8e080c0c31405a753b
MichalKukielkla <kukielka.michal@gmail.com>
2020-08-17 15:16:24 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -10,15 +10,34 @@ const App = () => {
 
   const [heroes, setHeroes] = useState([]);
   const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     const heroInfo = async () => {
-      const hero = await axios(`https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/1618520148304501/search/${query}`);
+      let cancel
+      const hero = await axios(
+        `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/1618520148304501/search/${query}`,
+        { cancelToken: new axios.CancelToken(c => cancel = c)}
+        );
       const result = JSON.parse(hero.request.responseText);
+      setLoading(false)
       setHeroes(result);
+      return () => cancel.cancel()
     };
     heroInfo();
   }, [query]);
+
+  if(loading) {
+    return (
+      <div className="container">
+      <Header />
+      <Search getQuery={(query) => setQuery(query)} />
+      <h1 className="center">Loading</h1>
+    </div>
+
+    )
+  }
 
   return (
     <div className="container">
@@ -28,5 +47,5 @@ const App = () => {
     </div>
   );
 }
-
+ 
 export default App;
